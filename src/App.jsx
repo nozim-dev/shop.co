70eebc60fff2bf7148da1d107775bcd4cb0d8b67
nozim-dev <nozimfordevelop@gmail.com>
2024-09-27 17:45:24 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <h1>Hello This Main Page</h1>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Cart</a>
+        </li>
+        <li>
+          <a href="">Category</a>
+        </li>
+        <li>
+          <a href="">Product Detail</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
