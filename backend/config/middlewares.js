758dece1d02966a52761c48163a405cfde6e22d2
nozim-dev <nozimfordevelop@gmail.com>
2024-10-09 11:34:29 +0500

    changed some file bug

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,24 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'storage.yandexcloud.net/strapi-static'],
+          'media-src': ["'self'", 'data:', 'blob:', 'storage.yandexcloud.net/strapi-static'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
